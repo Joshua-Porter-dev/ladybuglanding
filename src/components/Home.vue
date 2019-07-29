d1923e34a37bb8bfd07b36b1dabe406950dc32b4
joshua-porter-dev <jporter4117@gmail.com>
2019-07-29 00:31:57 -0500

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="home">
+      home page content
+   
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data(){
+      return{
+
+      }
+  }
+}
+</script>
+
+
+<style>
+</style>
---
+++ src/components/layouts/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="navbar">
+        <nav class="red darken-3">
+            <div class="container">
+                <a href="" class="brand-logo left">Ladybug Landing</a>
+                <ul class="right">
+                    <li><a href="">Signup</a></li>
+                    <li><a href="">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
